f77d06f7e3c2d3fbad04a8e716ba190e45068258
Is44C817 <davidisaac.sanchez.contractor@bbva.com>
2024-08-01 18:15:19 -0600

    add components form, header, carrusel

--- wallet/src/app/app.module.ts
+++ wallet/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WalletComponent } from './components/wallet/wallet.component';
 import { MaskCardNumberPipe } from './pipes/mask-card-number.pipe';
+import { FormComponent } from './components/form/form.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WalletComponent,
-    MaskCardNumberPipe
+    MaskCardNumberPipe,
+    FormComponent,
+    CarruselComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
