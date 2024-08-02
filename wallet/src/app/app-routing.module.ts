ed350f0c4b576cd8b397937b82077fd01913bd9f
Is44C817 <davidisaac.sanchez.contractor@bbva.com>
2024-08-01 22:35:36 -0600

    add routing and header

--- wallet/src/app/app-routing.module.ts
+++ wallet/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
+import { FormComponent } from './components/form/form.component';
+import { WalletComponent } from './components/wallet/wallet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FormComponent },
+  { path: 'carrusel', component: CarruselComponent },
+  { path: 'wallet', component: WalletComponent },
+  { path: '**', redirectTo: '' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
